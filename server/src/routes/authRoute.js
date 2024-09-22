200d500d8ff46282673ea6174f53721898cc3f5c
pradeep <pradeepbisen60@gmail.com>
2024-09-22 23:57:02 +0530

    cors fix

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -5,15 +5,15 @@ const {test,signup, signin, profile, logout, googleCallback}  = require("../cont
 const { googleSignin } = require("../controllers/authControl");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oidc")
-router.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:5173" || "http://localhost:5174",
-    // origin: "http://localhost:5174",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// router.use(
+//   cors({
+//     credentials: true,
+//     origin: "http://localhost:5173" || "http://localhost:5174",
+//     // origin: "http://localhost:5174",
+//     methods: ["GET", "POST"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 router.get("/", test);
 router.get('/login/federated/google',googleSignin);
 router.get("/auth/google/callback", passport.authenticate("google", { failureRedirect: "/" }), googleCallback);
